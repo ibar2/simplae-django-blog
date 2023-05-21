bacf5f039a9765e8641c335c889a3b8f68066dc5
ibar <ibar.student@simad.edu.so>
2023-05-21 10:51:26 +0300

    created a test for the model dunder str method using pytest and factoryboy

--- djblogger/blog/models.py
+++ djblogger/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    options = [
+        ('draft', 'Draft'),
+        ('published', "published")
+    ]
+
+    title = models.CharField(max_length=25)
+    subtitle = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=18)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_author')
+    content = models.TextField()
+    status = models.CharField(choices=options, max_length=10, default='draft')
+    createdat = models.DateField(auto_now_add=True, editable=False)
+    updataedat = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = '-createdat',
+
+    def __str__(self) -> str:
+        return self.title
