2747cd6b2e59768d74b3883f4af89462c22fe7f0
ibar <ibar.student@simad.edu.so>
2023-05-23 23:22:45 +0300

    removed the view func that was not needed and was used for testing

--- djblogger/blog/views.py
+++ djblogger/blog/views.py
@@ -4,10 +4,6 @@ from . import models
 # Create your views here.
 
 
-def home(req):
-    return render(req, 'blog/home.html')
-
-
 class homepage(ListView):
     model = models.Post
     template_name = 'blog/home.html'
