ffc16e3d875d47b4a793b28954f68401be6b3dc7
ibar <ibar.student@simad.edu.so>
2023-05-23 23:29:17 +0300

    created list_display for the post model in the admin panel

--- djblogger/blog/admin.py
+++ djblogger/blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib.auth.models import Group
+
+
+class PostMeta(admin.ModelAdmin):
+    list_display = ['title', 'author', 'status']
+
 
 admin.site.register(Post)
+admin.site.unregister(Group)
