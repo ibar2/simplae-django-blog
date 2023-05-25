9ff010a1ddd94e32d59ef8e58d711af81a285e81
ibar <ibar.student@simad.edu.so>
2023-05-25 15:38:27 +0300

    created a factory for the post models and used the factory to populate the database with 100 posts

---
+++ djblogger/blog/factory.py
@@ -0,0 +1,26 @@
+import factory
+from factory.faker import faker
+from django.contrib.auth.models import User
+from .models import Post
+
+
+fak = faker.Faker()
+
+
+class factoryPost(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = factory.Faker('sentence', nb_words=20)
+    subtitle = factory.Faker('sentence', nb_words=89)
+    slug = factory.Faker('slug')
+    author = User.objects.get_or_create(username='writer')[0]
+
+    @factory.lazy_attribute
+    def content(self):
+        x = ''
+        for _ in range(4):
+            x += '\n' + fak.paragraph(nb_sentences=50) + '\n'
+            return x
+
+    status = 'published'
