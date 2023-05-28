080fd0d2b6115c16cb38c6dd9d4cb6ec7e375713
ibar <ibar.student@simad.edu.so>
2023-05-28 23:09:51 +0300

    added the single post template testing to our existing class that tested the status code of the response from single post

--- djblogger/Test/blog/test_single.py
+++ djblogger/Test/blog/test_single.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_django.asserts import assertTemplateUsed
 from django.urls import reverse
 
 pytestmark = pytest.mark.django_db
@@ -10,3 +11,9 @@ class TestSinglePost:
         url = reverse('post-single', kwargs={'post': post.slug})
         res = client.get(url)
         assert res.status_code == 200
+
+    def test_singlepost_template(self, client, post_factory):
+        post = post_factory()
+        url = reverse('post-single', kwargs={'post': post.slug})
+        res = client.get(url)
+        assertTemplateUsed(res, 'blog/single_post.html')
